b231eaaf2f767fde74555b973a13a62fe53b301a
Pavel <pashicop@gmail.com>
2022-03-22 14:18:30 +0300

    Complete

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from pprint import pprint
+import pathlib
+from pathlib import Path
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+
+HEADERS = {"Authorization": "OAuth AQAAAAAWTeeYAADLW0-rjvA3UE1bqNEUIgDXDu4"}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path):
+        """Метод загруджает файл file_path на яндекс диск"""
+        response = requests.get("https://cloud-api.yandex.net/v1/disk/resources/upload",
+                                params={"path": os.path.basename(file_path), "overwrite": "true"},
+                                headers=HEADERS
+                                )
+        response.raise_for_status()
+        data = response.json()
+        # pprint(data)
+        href = data["href"]
+        with open(file_path, "rb") as f:
+            put_response = requests.put(href, files={"file": f}, headers=HEADERS)
+            put_response.raise_for_status()
+            # pprint(put_response.text)
+        return 'Файл успешно загружен'
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uploader = YaUploader('AQAAAAAWTeeYAADLW0-rjvA3UE1bqNEUIgDXDu4')
+    dir_path = Path.home()
+    f_path = Path(dir_path, "Downloads", "1.docx")
+    print(f_path)
+    result = uploader.upload(f_path)
+    print(result)
